d7011cb0e2545d94ef62e88785631098b3457604
GoOcto <paul@goocto.com>
2025-10-23 01:55:17 -0600

    chore: rename script to a more logical name

---
+++ scripts/download_colormap.sh
@@ -0,0 +1,5 @@
+# high-res
+wget https://assets.science.nasa.gov/content/dam/science/psd/lunar-science/2023/09/colormap.jpg .data/color/colormap.jpg
+
+# very high-res
+wget https://lroc.im-ldi.com/ptif/download_file?fName=BasicHapke_wbhs_blend_b137_IOF.55deg_nearcenter_1kmpp_20140925_134515.tif .data/color/colormap_1kmpp.tif
--- scripts/download_lowres_color.sh
+++
@@ -1,1 +0,0 @@
-wget https://assets.science.nasa.gov/content/dam/science/psd/lunar-science/2023/09/colormap.jpg .data/color/colormap.jpg
